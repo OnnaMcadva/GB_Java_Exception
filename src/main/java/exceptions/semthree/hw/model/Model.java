54d24ae78b3f7fc14a97477fc58bea7b8f1070b7
Onna Mcadva <anna.anutka@gmail.com>
2023-09-30 20:54:42 +0200

    sem 3

--- src/main/java/exceptions/semthree/hw/model/Model.java
+++ src/main/java/exceptions/semthree/hw/model/Model.java
@@ -6,8 +6,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
